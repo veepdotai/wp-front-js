45cdecb21e1fc4a66ac326728b2c01f49bd3a446
CarreauTh <tho.carreau@gmail.com>
2023-08-28 10:36:59 +0200

    Refactor: adding a function to make the request to Pexels

--- API/testApi.js
+++ API/testApi.js
@@ -1,21 +1,26 @@
 const apiKey = 'wq2JAMdIYANS25GZSqH2qbA3tFS9nSogifigVIJf7KzSEUfh12k9VtBB';
 const searchQuery = 'Paysage'; // Remplace par le mot-clé de ton choix
 const apiUrl = `https://api.pexels.com/v1/search?query=${searchQuery}&per_page=10`; // Modifier le nombre d'images si nécessaire
+const jsonTest = './test.json';
 
-fetch(apiUrl, {
-    headers: {
-        Authorization: apiKey,
-    },
-})
-.then(response => response.json())
-.then(data => {
-    const imageContainer = document.getElementById('image-container');
+pexelsRequest();
 
-    data.photos.forEach(photo => {
-        const imgElement = document.createElement('img');
-        imgElement.src = photo.src.landscape; // Ou photo.src.large si tu veux une plus grande résolution
-        imgElement.alt = photo.photographer;
-        imageContainer.appendChild(imgElement);
-    });
-})
-.catch(error => console.error('Erreur :', error));
+function pexelsRequest() {
+    fetch(apiUrl, {
+        headers: {
+            Authorization: apiKey,
+        },
+    })
+    .then(response => response.json())
+    .then(data => {
+        const imageContainer = document.getElementById('image-container');
+
+        data.photos.forEach(photo => {
+            const imgElement = document.createElement('img');
+            imgElement.src = photo.src.landscape; // Ou photo.src.large si tu veux une plus grande résolution
+            imgElement.alt = photo.photographer;
+            imageContainer.appendChild(imgElement);
+        });
+    })
+    .catch(error => console.error('Erreur :', error));
+}
