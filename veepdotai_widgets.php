8009cb749f71aeb9c684ba791f65ddd9c8c2615e
CarreauTh <tho.carreau@gmail.com>
2023-11-15 16:22:41 +0100

    Refactor: new constant for api keys

--- veepdotai_widgets.php
+++ veepdotai_widgets.php
@@ -17,81 +17,85 @@ require 'vendor/autoload.php';
 use ImgFinder\Repository\PexelsRepository;
 use ImgFinder\Repository\UnsplashRepository;
 
-// Don't forget to replace by your api's keys
-define('PEXELS_API_KEY', PEXELS_API_KEY); 
-define('UNSPLASH_API_KEY', UNSPLASH_API_KEY);
-
-if (! defined('WPINC')) {
-    die;
-}
+// Don't forget to replace by your api's key
+define('PEXELS_API_KEY', 'MY_OWN_PERSONAL_PEXELS_KEY'); // value to change
+define('UNSPLASH_API_KEY', 'MY_OWN_PERSONAL_UNSPLASH_KEY'); // value to change
 
 define('VEEPDOTAI_WIDGETS_VERSION', '1.0.0');
 define('VEEPDOTAI_WIDGETS_PLUGIN_VERSION', '1.0.0');
 
-function veepdotai_widgets_enqueue_style() {
-    wp_enqueue_style( 'cdn-css-jquery.modal', 'https://cdnjs.cloudflare.com/ajax/libs/jquery-modal/0.9.1/jquery.modal.min.css', false);
-    wp_enqueue_style( 'splidejs-css-splide-4.1.4', 'https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.4/dist/css/splide.min.css', false );
-    wp_enqueue_style( 'my-veepdotai-widgets-css-carousel', plugins_url('/veepdotai_widgets/admin/assets/carousel/css/carousel.css'), false );
-    wp_enqueue_style( 'my-veepdotai-widgets-css-inline-editor' , plugins_url('/veepdotai_widgets/admin/assets/inline-editor/css/inline-editor.css'), false);
-}                                                                                               
+if (!defined('WPINC')) {
+    die;
+}
 
-function veepdotai_widgets_enqueue_script() {
-    wp_enqueue_script( 'cdn-splidejs-js-splide-4.1.4', 'https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.4/dist/js/splide.min.js', true );
-    wp_enqueue_script( 'my-veepdotai-widgets-js-jquery-3.7.1', plugins_url( '/veepdotai_widgets/admin/assets/carousel/js/jquery-3.7.1.js'), true );
-    wp_enqueue_script( 'cdn-js-jquery.modal', 'https://cdnjs.cloudflare.com/ajax/libs/jquery-modal/0.9.1/jquery.modal.min.js', true );
+function veepdotai_widgets_enqueue_style()
+{
+    wp_enqueue_style('cdn-css-jquery.modal', 'https://cdnjs.cloudflare.com/ajax/libs/jquery-modal/0.9.1/jquery.modal.min.css', false);
+    wp_enqueue_style('splidejs-css-splide-4.1.4', 'https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.4/dist/css/splide.min.css', false);
+    wp_enqueue_style('my-veepdotai-widgets-css-carousel', plugins_url('/veepdotai_widgets/admin/assets/carousel/css/carousel.css'), false);
+    wp_enqueue_style('my-veepdotai-widgets-css-inline-editor', plugins_url('/veepdotai_widgets/admin/assets/inline-editor/css/inline-editor.css'), false);
+}
 
-    wp_enqueue_script( 'my-veepdotai-widgets-js-loader', plugins_url( '/veepdotai_widgets/admin/assets/carousel/js/loader.js' ), true );
-    wp_enqueue_script( 'my-veepdotai-widgets-js-carousel', plugins_url( '/veepdotai_widgets/admin/assets/carousel/js/carousel.js' ), true );
-    wp_enqueue_script( 'my-veepdotai-widgets-js-staticJson', plugins_url( '/veepdotai_widgets/admin/assets/carousel/js/staticJson.js' ), true );
+function veepdotai_widgets_enqueue_script()
+{
+    wp_enqueue_script('cdn-splidejs-js-splide-4.1.4', 'https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.4/dist/js/splide.min.js', true);
+    wp_enqueue_script('my-veepdotai-widgets-js-jquery-3.7.1', plugins_url('/veepdotai_widgets/admin/assets/carousel/js/jquery-3.7.1.js'), true);
+    wp_enqueue_script('cdn-js-jquery.modal', 'https://cdnjs.cloudflare.com/ajax/libs/jquery-modal/0.9.1/jquery.modal.min.js', true);
 
-    wp_enqueue_script( 'my-veepdotai-widgets-js-inline-editor', plugins_url( '/veepdotai_widgets/admin/assets/inline-editor/js/inline-editor.js' ), true );
+    wp_enqueue_script('my-veepdotai-widgets-js-loader', plugins_url('/veepdotai_widgets/admin/assets/carousel/js/loader.js'), true);
+    wp_enqueue_script('my-veepdotai-widgets-js-carousel', plugins_url('/veepdotai_widgets/admin/assets/carousel/js/carousel.js'), true);
+    wp_enqueue_script('my-veepdotai-widgets-js-staticJson', plugins_url('/veepdotai_widgets/admin/assets/carousel/js/staticJson.js'), true);
+
+    wp_enqueue_script('my-veepdotai-widgets-js-inline-editor', plugins_url('/veepdotai_widgets/admin/assets/inline-editor/js/inline-editor.js'), true);
 
     wp_localize_script(
         'my-veepdotai-widgets-js-loader',
         'MyAjax',
         array(
-            'ajaxurl'  => admin_url( 'admin-ajax.php' ),
-            'security' => wp_create_nonce( 'my-special-string' ),
+            'ajaxurl'  => admin_url('admin-ajax.php'),
+            'security' => wp_create_nonce('my-special-string'),
         )
     );
 }
 
-add_action( 'wp_enqueue_scripts', 'veepdotai_widgets_enqueue_style' );
-add_action( 'wp_enqueue_scripts', 'veepdotai_widgets_enqueue_script' );
+add_action('wp_enqueue_scripts', 'veepdotai_widgets_enqueue_style');
+add_action('wp_enqueue_scripts', 'veepdotai_widgets_enqueue_script');
 
-add_action( 'wp_ajax_save_featured_image', 'save_featured_image_callback' );
-add_action( 'wp_ajax_save_article_inline', 'save_article_inline_callback' );
+add_action('wp_ajax_save_featured_image', 'save_featured_image_callback');
+add_action('wp_ajax_save_article_inline', 'save_article_inline_callback');
 
-add_action( 'wp_ajax_get_json_api', 'get_json_api_callback' );
+add_action('wp_ajax_get_json_api', 'get_json_api_callback');
 
-function save_featured_image_callback(){
+function save_featured_image_callback()
+{
     $url = $_POST['src'];
     $alt = $_POST['alt'];
     $postId = $_POST['postId'];
     $isUnsplash = $_POST['isUnsplash'];
-    
-    if ($isUnsplash === "true"){
+
+    if ($isUnsplash === "true") {
         $url = $url . '&.png';
     }
 
     $isAlreadyLoaded = isAlreadyLoaded($url);
-    
-    if ($isAlreadyLoaded){
+
+    if ($isAlreadyLoaded) {
         $imageId = $isAlreadyLoaded;
-    }else {
-        $imageId = media_sideload_image( $url, 0, $url, 'id' );
+    } else {
+        $imageId = media_sideload_image($url, 0, $url, 'id');
     }
 
-    if (is_int($imageId)){
+    if (is_int($imageId)) {
         update_post_meta($imageId, '_wp_attachment_image_alt', $alt);
         set_post_thumbnail($postId, $imageId);
     }
-    
+
     echo $isAlreadyLoaded;
     die;
 }
 
-function save_article_inline_callback(){
+function save_article_inline_callback()
+{
     $content = $_POST['content'];
     $postId = $_POST['postId'];
 
@@ -106,10 +110,8 @@ function save_article_inline_callback(){
     die;
 }
 
-function get_json_api_callback(){
-    $pexelsKey = PEXELS_API_KEY; 
-    $unsplashKey = UNSPLASH_API_KEY;
-    
+function get_json_api_callback()
+{
     $nbImage = 2;
 
     $query = $_POST["query"];
@@ -120,38 +122,39 @@ function get_json_api_callback(){
             'repositories' => [
                 PexelsRepository::class => [
                     'params' => [
-                        'authorization' => $pexelsKey
+                        'authorization' => constant('PEXELS_API_KEY')
                     ]
                 ],
                 UnsplashRepository::class => [
                     'params' => [
-                        'authorization' => $unsplashKey
+                        'authorization' => constant('UNSPLASH_API_KEY')
                     ]
                 ]
             ]
         ]
     ];
-    
+
     $config = ImgFinder\Config::fromArray($settings);
-    
+
     $finder = new ImgFinder\ImgFinder($config);
-    
+
     if ($api[0] === "both") {
-        $api = ['pexels','unsplash'];
-    }else{
+        $api = ['pexels', 'unsplash'];
+    } else {
         $nbImage *= 2;
     }
-    
+
     $request = ImgFinder\Request::set($query, $api, 1, $nbImage, 'landscape', 1200, 320);
     $response = $finder->search($request);
-    
+
     $imagesUrls = $response->toArray();
-    
+
     echo json_encode($imagesUrls);
     die;
 }
 
-function isAlreadyLoaded($newUrl){
+function isAlreadyLoaded($newUrl)
+{
     $args = array(
         'post_type' => 'attachment',
         'post_status' => 'any',
@@ -159,25 +162,23 @@ function isAlreadyLoaded($newUrl){
     );
 
     $query = new WP_Query($args);
-    
+
     $newImageId = 0;
-    
-    if($query->have_posts()) {
+
+    if ($query->have_posts()) {
         while ($query->have_posts()) {
             $query->the_post();
 
             $imageId = get_the_ID();
-            
+
             $urlSource = get_post_meta($imageId, '_source_url')[0];
-            $urlSource = str_replace("\\","",$urlSource);
-            
-            if ($urlSource === $newUrl){
-                $newImageId = $imageId; 
+            $urlSource = str_replace("\\", "", $urlSource);
+
+            if ($urlSource === $newUrl) {
+                $newImageId = $imageId;
             }
         }
     }
-    
+
     return $newImageId;
 }
-
-?>
